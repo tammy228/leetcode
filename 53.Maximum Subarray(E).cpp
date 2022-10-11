7444f4d2cb4a5f909ae687e210edb19fdb209c31
tammy <saintpaulstammy@gmail.com>
2022-10-11 21:53:29 +0800

    53. Change for loop to iterator, faster the runtime.

--- 53.Maximum Subarray(E).cpp
+++ 53.Maximum Subarray(E).cpp
@@ -1,23 +1,34 @@
 #include<iostream>
 #include<vector>
+#include<climits>
 using namespace std;
 /*
  * My Solution
- * 1. iterate the array and keep add the element
- * 2. if prefixSum <= 0: prefixSum = 0
- *    else prefixSum += element
- *
+ * 1. There will be variable called "presum" which will accumulate the number while iterating the array.
+ * 2. There will be another variable called "maxsum" which store the largest number of presum.
+ * 3. while iterating the array:
+ *    if (presum >= maxsum) maxsum = presum
+ *    if (presum <= 0) presum = 0
+ * Edge Case:
+ * 1. if only one element, return nums[0]
  * Time: O(n)
  */
 int maxSubArray(vector<int>& nums) {
     if(nums.size() == 1) return nums[0];
-    int ans=nums[0];
-    int prefixSum=0;
-    for(int i=1; i<nums.size(); i++){
-        if(prefixSum <= 0)
-            prefixSum = 0;
-        prefixSum += nums[i];
-        ans = max(ans, prefixSum);
+    int maxsum = INT_MIN;
+    int presum = 0;
+//    for(int i=0; i<nums.size(); i++){
+//        presum += nums[i];
+//        if(presum > maxsum) maxsum = presum;
+//        // basically if presum is smaller than 0,
+//        if(presum <= 0) presum = 0;
+//    };
+    // Using iterator for traversal (iterator is always faster)
+    for(auto i: nums){
+        presum += i;
+        if (presum > maxsum) maxsum = presum;
+        if(presum <= 0) presum = 0;
     }
-    return ans;
+
+    return maxsum;
 }
