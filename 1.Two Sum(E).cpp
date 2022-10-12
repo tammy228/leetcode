cb82ddbf354d74c1f7453deec5f63b8094433b97
tammy <saintpaulstammy@gmail.com>
2022-10-12 21:53:22 +0800

    1. Add comment and refactor the code.

--- 1.Two Sum(E).cpp
+++ 1.Two Sum(E).cpp
@@ -2,17 +2,25 @@
 #include <vector>
 #include <unordered_map>
 using namespace std;
+/*
+ * My Solution
+ * use ordered_map to store info, first: the number of "target - nums[i]", second: the index of that nums[i]
+ *
+ * 1. while iterating the nums, store the info into map
+ * 2. meanwhile, check if nums[i] exists in map.
+ *    if true: return {i, maps[nums[i]]}
+ *
+ * Edge Case: Nope
+ *
+ * Time: O(n)
+ * Space: O(n)
+ */
 vector<int> twoSum(vector<int>& nums, int target) {
-    unordered_map<int, int> um;
-    int key;
-    for(int i = 0; i < nums.size(); i++){
-        key = nums[i];
-        if(um.count(target - key) == 0){
-            um[key] = i;
-        }
-        else{
-            return { i, um[target - key]};
-        }
+    unordered_map<int, int> map;
+    for(int i=0; i<nums.size(); i++){
+        if(map.find(nums[i]) != map.end())
+            return {i, map[nums[i]]};
+        map[target-nums[i]] = i;
     }
     return {};
 }
