3fc981b2ab9f8243bad39fe95814bcdb0195f358
tammy228 <saintpaulstammy@gmail.com>
2022-12-02 09:19:35 +0800

    Update 57.

--- 57.Insert Interval(M).cpp
+++ 57.Insert Interval(M).cpp
@@ -20,15 +20,18 @@ using namespace std;
  * O(1): output vector doesn't count as extra space.
  */
 vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-    vecotor<vecotr<int>> result;
+    vector<vector<int>> result;
     for(int i=0; i<intervals.size(); i++){
-
         if(newInterval[1] < intervals[i][0]){
             result.push_back(newInterval);
-        }else if(intervals[i][1] < newInterval[0]){
+            newInterval = intervals[i];
+        }else if(newInterval[0] > intervals[i][1]){
             result.push_back(intervals[i]);
         }else{
-
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
         }
     }
+    result.push_back(newInterval);
+    return result;
 }
