50123c8d313cd00d2e9fc34f047d42546bcf2e24
tammy228 <saintpaulstammy@gmail.com>
2022-12-14 08:01:42 +0800

    Update 15. of all solution.

--- 15.3Sum(M).cpp
+++ 15.3Sum(M).cpp
@@ -1,59 +1,85 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <set>
-#include <tuple>
+#include <climits>
 using namespace std;
-// My Solution O(n^2)
-//vector<vector<int>> threeSum(vector<int>& nums) {
-//    vector<vector<int>> ans,tmp;
-//    sort(nums.begin(), nums.end());
-//    int x,two_sum;
-//    set<vector<int>> duplicate;
-//    for(int i=0; i<nums.size(); i++){
-//        if(i>0 && nums[i]==nums[i-1])
-//            continue;
-//        x = nums[i];
-//        two_sum = 0 - x;
-//        set<int> two_sum_st;
-//        for(int j=i+1; j<nums.size(); j++){
-//            if(two_sum_st.count(two_sum-nums[j]) == 0)
-//                two_sum_st.insert(nums[j]);
-//            else
-//                if(duplicate.count({x, two_sum-nums[j], nums[j]}) == 0){
-//                    duplicate.insert({x, two_sum-nums[j], nums[j]});
-//                    ans.push_back({x, two_sum - nums[j], nums[j]});
-//                }
-//        }
-//    }
-//    return ans;
-//}
-
-// Optimize
-vector<vector<int>> threeSum(vector<int>& nums){
-    int n = nums.size();
-    if(n<3)
-        return {};
-    vector<vector<int>> ans;
+/*
+ * My Solution
+ * Apply "Two Sum" for N times
+ *
+ * Time:
+ * O(N^2): where N = nums.size()
+ *
+ * Space:
+ * O(N): where N = nums.size()
+ */
+vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> result;
+    int target = INT_MIN;
     sort(nums.begin(), nums.end());
-    for(int i=0; i<n-2; i++){
-        if(i>0 && nums[i]==nums[i-1]) continue;
-        int j = i+1, k = n-1;
-        while(j<k){
-            int sum = nums[i] + nums[j] + nums[k];
-            if(sum==0){
-                ans.push_back({nums[i], nums[j], nums[k]});
-                while(j<k && nums[j] == nums[j+1]) j++;
-                while(j<k && nums[k] == nums[k-1]) k--;
-                j++;
-                k--;
+    for(int i=0; i<nums.size(); i++){
+        vector<int> combinations;
+        if(target != -nums[i]) {
+            unordered_set<int> set;
+            target = -nums[i];
+            int tmp = INT_MIN;
+            for (int j = i+1; j < nums.size(); j++) {
+                if(tmp != nums[j]) {
+                    if (set.find(nums[j]) != set.end()) {
+                        tmp = nums[j];
+                        combinations.push_back(nums[j]);
+                        combinations.push_back(target - nums[j]);
+                        combinations.push_back(nums[i]);
+                        result.push_back(combinations);
+                        combinations.clear();
+                    }
+                    set.insert(target - nums[j]);
+                }
             }
-            else if(sum > 0) k--;
-            else j++;
-
         }
     }
-    return ans;
+    return result;
+}
+
+
+/*
+ * Optimize
+ * 1. Sort the array to prevent duplicate problem
+ * 2. Use "Two pointers" to find sum, during the finding if pointers point to same value as before, then shift it.
+ *
+ * Time:
+ * O(N^2)
+ *
+ * Space:
+ * O(1)
+ */
+vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> result;
+    // 1.
+    sort(nums.begin(), nums.end());
+    for(int i=0; i<nums.size()-2; i++){
+        // prevent use duplicate of first number
+        if(i > 0 && nums[i] == nums[i-1])
+            continue;
+
+        int left = i + 1;
+        int right = nums.size()-1;
+        // 2.
+        while(left < right){
+            int threeSum = nums[i] + nums[left] + nums[right];
+            if(threeSum > 0) {
+                right--;
+            }else if(threeSum < 0) {
+                left++;
+            }else{
+                result.push_back({nums[i], nums[left], nums[right]});
+                left++;
+                // only check one of the pointer, because the outer while loop is going to check the other pointer
+                // always remember if there is another while loop inside while loop, be careful with the condition
+                while(left < right && nums[left] == nums[left-1]) left++;
+            }
+        }
+    }
+    return result;
 }
 
 //int main(){
