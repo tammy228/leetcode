cf158cae8c1ca575ca9daf9ce3554626fd606bfb
tammy228 <saintpaulstammy@gmail.com>
2023-01-09 09:21:22 +0800

    Update 206.

--- 206.Reverse Linked List(E).cpp
+++ 206.Reverse Linked List(E).cpp
@@ -1,3 +1,25 @@
-//
-// Created by Tammy on 2022/12/30.
-//
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+/*
+ * Time: O(N)
+ *
+ * Space: O(1)
+ */
+ListNode* reverseList(ListNode* head) {
+    ListNode* curr = head;
+    ListNode* prev = nullptr;
+    while(head){
+        curr = head->next;
+        head->next = prev;
+        prev = head;
+        head = curr;
+    }
+    return head;
+}
