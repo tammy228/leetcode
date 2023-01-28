0719c36c53a425cc09c26a178711cf9c723e80f2
tammy228 <saintpaulstammy@gmail.com>
2023-01-28 22:35:51 +0800

    Update 876.

--- 876.Middle of the Linked List(E).cpp
+++ 876.Middle of the Linked List(E).cpp
@@ -1,3 +1,27 @@
-//
-// Created by Tammy on 2023/1/19.
-//
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+/*
+ * My Solution
+ * Use two for loop, one for count size, and then take size/2 to get the result.
+ */
+ListNode* middleNode(ListNode* head) {
+    int size = 0;
+    ListNode* dummyHead = head;
+    ListNode* result = head;
+    while(dummyHead != nullptr){
+        dummyHead = dummyHead->next;
+        size++;
+    }
+    int mid = size % 2 == 0 ? size/2+1 : size/2;
+    for(int i=0; i<mid; i++){
+        result = result->next;
+    }
+
+    return result;
+}
