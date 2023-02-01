c0d2603577c00a2a231f9e53ea1aa38cc765d5a1
tammy228 <saintpaulstammy@gmail.com>
2023-02-01 08:09:30 +0800

    Add 46.

---
+++ 46.Permutations(M).cpp
@@ -0,0 +1,23 @@
+#include <vector>
+using namespace std;
+/*
+ * My Solution
+ * (Backtracking)
+ * All permutation combination, if we have 3 integers, is 3*2*1 = 6
+ * Use swap to achieve permutation
+ *
+ */
+void solve(vector<int>& nums, vector<vector<int>>& result, int begin){
+    if(begin == nums.size())
+        result.push_back(nums);
+    for(int i=begin; i<nums.size(); i++){
+        swap(nums[i], nums[begin]);
+        solve(nums, result, begin+1);
+        swap(nums[i], nums[begin]);
+    }
+}
+vector<vector<int>> permute(vector<int>& nums) {
+    vector<vector<int>> result;
+    solve(nums, result, 0);
+    return result;
+}
