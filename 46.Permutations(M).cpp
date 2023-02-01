685c7118fb7ba044d400b86fa149a9e6cf8f41d4
tammy228 <saintpaulstammy@gmail.com>
2023-02-02 07:38:10 +0800

    Update 46. time and sapce complexity.

--- 46.Permutations(M).cpp
+++ 46.Permutations(M).cpp
@@ -6,6 +6,11 @@ using namespace std;
  * All permutation combination, if we have 3 integers, is 3*2*1 = 6
  * Use swap to achieve permutation
  *
+ * Time:
+ * O(n!)
+ *
+ * Space:
+ * O(n!): calling stack
  */
 void solve(vector<int>& nums, vector<vector<int>>& result, int begin){
     if(begin == nums.size())
