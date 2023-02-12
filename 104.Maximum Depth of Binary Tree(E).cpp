d1ff0d61f244df1578c20e840d882769f2e10ee0
tammy228 <saintpaulstammy@gmail.com>
2023-02-12 14:03:03 +0800

    Update 104.

--- 104.Maximum Depth of Binary Tree(E).cpp
+++ 104.Maximum Depth of Binary Tree(E).cpp
@@ -1,3 +1,25 @@
-//
-// Created by Tammy on 2023/2/12.
-//
+#include <iostream>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+int dfs(TreeNode* root){
+    if(root == NULL)
+        return 0;
+
+    int left = dfs(root->left);
+    int right = dfs(root->right);
+
+    return max(left, right)+1;
+
+
+}
+int maxDepth(TreeNode* root) {
+
+    return dfs(root);
+}
