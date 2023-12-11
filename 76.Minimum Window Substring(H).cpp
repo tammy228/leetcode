0ad3bcb3815b0ca899086c423960414bada0d135
tammy228 <saintpaulstammy@gmail.com>
2023-12-11 21:02:48 +0800

    Update 76. for sliding window template

--- 76.Minimum Window Substring(H).cpp
+++ 76.Minimum Window Substring(H).cpp
@@ -48,36 +48,70 @@ using namespace std;
  * Space:
  * O(N): N for t.size()
  */
+//string minWindow(string s, string t) {
+//    if(t.size() > s.size()) return "";
+//    unordered_map<char, int> map;
+//    for(char s : t){
+//        map[s]++;
+//    }
+//
+//    int left=0, right=0;
+//    int shortest_length = INT_MAX;
+//    int valid_count = map.size();
+//    int start = 0;
+//    while(right < s.size()){
+//        map[s[right]]--;
+//        if(map[s[right]] == 0)
+//            valid_count--;
+//
+//        while(valid_count == 0){
+//            if (right - left + 1 < shortest_length) {
+//                shortest_length = right - left + 1;
+//                start = left;
+//            }
+//            map[s[left]]++;
+//            if(map[s[left]] > 0)
+//                valid_count++;
+//            left++;
+//        }
+//        right++;
+//    }
+//    if(shortest_length != INT_MAX)
+//        return s.substr(start, shortest_length);
+//    else
+//        return "";
+//}
+/*
+ * It's a sliding window template
+ * First we will have two pointer counter for restriction and ans
+ * In the outer while loop (for "right" pointer to iterate)
+ *      modify the counter(for whatever the problem's condition)
+ * In the inner while loop (for "left" pointer to iterate)
+ *      update ans if the problem is search for "min"
+ *      modify the counter
+ * Outside inner while loop
+ *      update ans if the problem is search for "MAX"
+ *
+ */
 string minWindow(string s, string t) {
-    if(t.size() > s.size()) return "";
     unordered_map<char, int> map;
-    for(char s : t){
-        map[s]++;
-    }
-
-    int left=0, right=0;
-    int shortest_length = INT_MAX;
-    int valid_count = map.size();
-    int start = 0;
-    while(right < s.size()){
-        map[s[right]]--;
-        if(map[s[right]] == 0)
-            valid_count--;
-
-        while(valid_count == 0){
-            if (right - left + 1 < shortest_length) {
-                shortest_length = right - left + 1;
-                start = left;
+    // vector<int> map(128,0);
+    int counter = t.size(), left = 0, right = 0, head = 0, ans = INT_MAX;
+    for (int i = 0; i < t.size(); i++)
+        map[t[i]]++;
+    while(right < s.size())
+    {
+        if(map[s[right]]-- > 0) counter--;
+        right++;
+        while(counter == 0)
+        {
+            if (right-left < ans){
+                ans = right - left;
+                head = left;
             }
-            map[s[left]]++;
-            if(map[s[left]] > 0)
-                valid_count++;
+            if (map[s[left]]++ == 0) counter++;
             left++;
         }
-        right++;
     }
-    if(shortest_length != INT_MAX)
-        return s.substr(start, shortest_length);
-    else
-        return "";
+    return ans == INT_MAX ? "" : s.substr(head, ans);
 }
