fd988fb97873f2b5c926b430405418fb1adbaeda
tammy228 <saintpaulstammy@gmail.com>
2024-03-13 20:47:22 +0800

    Update 62.

--- 62.Unique Paths(M).cpp
+++ 62.Unique Paths(M).cpp
@@ -1,25 +1,18 @@
-#include<iostream>
 #include<vector>
 using namespace std;
 /*
  * My Solution
- * *(DP, DFS)
- * *(DFS will cause time limit)
+ * DP
+ *
  */
-int uniquePaths_dfs(int m, int n, int x, int y, vector<vector<int>> & dp){
-    if(x > m || y > n)
-        return 0;
-
-    // if the cell is already calculated, return it's value.
-    if(dp[x][y] != -1)
-        return dp[x][y];
-    dp[x][y] = uniquePaths_dfs(m , n, x+1, y, dp) + uniquePaths_dfs(m , n, x, y+1, dp);
-
-    return dp[x][y];
-
-}
 int uniquePaths(int m, int n) {
-    vector<vector<int>> dp (m+1, vector<int>(n+1,-1));
-    dp[m][n] = 1;
-    return uniquePaths_dfs(m, n, 0, 0, dp);;
+    vector<vector<int>> dp(m, vector<int>(n, 1));
+
+    for(int i=1; i<m; i++){
+        for(int j=1; j<n; j++){
+            dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        }
+    }
+
+    return dp[m-1][n-1];
 }
