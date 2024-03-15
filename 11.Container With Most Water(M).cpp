8bb182e51c7d34cc497a7915f7d9553aa7c1a290
tammy228 <saintpaulstammy@gmail.com>
2024-03-15 22:42:53 +0800

    Update 11. solution

--- 11.Container With Most Water(M).cpp
+++ 11.Container With Most Water(M).cpp
@@ -9,24 +9,40 @@ using namespace std;
  *    else right --
  *
  * Time: O(n)
+ * Space: O(1)
  *
- * Edge Case:
- * 1. Only two element: return smaller height
  */
+//int maxArea(vector<int>& height) {
+//    if(height.size() == 2) return height[0] > height[1] ? height[1] : height[0];
+//    int left=0, right=height.size()-1;
+//    int ans=0;
+//    while(left < right){
+//        int area = (right-left) * (min(height[right],height[left]));
+//        ans = max(area, ans);
+//        if(height[left] <= height[right]){
+//            while(height[left+1] <= height[left] && left < right) left++;
+//            left++;
+//        }else{
+//            while(height[right-1] <= height[right] && left < right) right--;
+//            right--;
+//        }
+//    }
+//    return ans;
+//}
+
 int maxArea(vector<int>& height) {
-    if(height.size() == 2) return height[0] > height[1] ? height[1] : height[0];
-    int left=0, right=height.size()-1;
-    int ans=0;
-    while(left < right){
-        int area = (right-left) * (min(height[right],height[left]));
-        ans = max(area, ans);
-        if(height[left] <= height[right]){
-            while(height[left+1] <= height[left] && left < right) left++;
+    int left = 0;
+    int right = height.size() - 1;
+    int ans = 0;
+    while(left < right)
+    {
+        int area = (right - left) * min(height[left], height[right]);
+        ans = area > ans ? area : ans;
+
+        if(height[left] <= height[right])
             left++;
-        }else{
-            while(height[right-1] <= height[right] && left < right) right--;
+        else
             right--;
-        }
     }
     return ans;
 }
