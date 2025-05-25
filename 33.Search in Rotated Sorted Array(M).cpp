ee96d0754478e6b6015219cc5e312eb806a6800b
tammy228 <saintpaulstammy@gmail.com>
2025-05-25 09:48:48 +0800

    Update 33.

--- 33.Search in Rotated Sorted Array(M).cpp
+++ 33.Search in Rotated Sorted Array(M).cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 /* My Solution(Fail):
  * 1. find k
@@ -8,115 +8,86 @@ using namespace std;
  * Edge Case: Only one element
  * Problems: [1,3] it will error if it is sorted.
  */
-//int binarySearch(vector<int> &new_nums, int target){
-//    int left=0, right=new_nums.size();
-//    int result = -1;
-//    while(left <= right) {
-//        int middle = (left + right) / 2;
-//        if (new_nums[middle] > target)
-//            right = middle - 1;
-//        else if (new_nums[middle] < target)
-//            left = middle + 1;
-//        else {
-//            result = middle;
-//            break;
-//        }
-//    }
-//    return result;
-//}
-//int search(vector<int>& nums, int target) {
-//    if(nums.size() == 1)
-//        return binarySearch(nums, target);
-//    int biggest = INT_MIN;
-//    int index;
-//    // 1.
-//    for(int i=0; i<nums.size(); i++){
-//        if(nums[i] > biggest) {
-//            biggest = nums[i];
-//            index = i;
-//        }
-//    }
+// int binarySearch(vector<int> &new_nums, int target){
+//     int left=0, right=new_nums.size();
+//     int result = -1;
+//     while(left <= right) {
+//         int middle = (left + right) / 2;
+//         if (new_nums[middle] > target)
+//             right = middle - 1;
+//         else if (new_nums[middle] < target)
+//             left = middle + 1;
+//         else {
+//             result = middle;
+//             break;
+//         }
+//     }
+//     return result;
+// }
+// int search(vector<int>& nums, int target) {
+//     if(nums.size() == 1)
+//         return binarySearch(nums, target);
+//     int biggest = INT_MIN;
+//     int index;
+//     // 1.
+//     for(int i=0; i<nums.size(); i++){
+//         if(nums[i] > biggest) {
+//             biggest = nums[i];
+//             index = i;
+//         }
+//     }
 //
-//    // 2.
-//    vector<int> new_nums(nums.size());
-//    // new_nums[0~index]
-//    for(int i=index+1, tmp=0; i<nums.size(); i++, tmp++)
-//        new_nums[tmp] = nums[i];
-//    // new_nums[index~nums.size()]
-//    for(int i=0, tmp=nums.size()-index-1; i<=index; i++, tmp++)
-//        new_nums[tmp] = nums[i];
+//     // 2.
+//     vector<int> new_nums(nums.size());
+//     // new_nums[0~index]
+//     for(int i=index+1, tmp=0; i<nums.size(); i++, tmp++)
+//         new_nums[tmp] = nums[i];
+//     // new_nums[index~nums.size()]
+//     for(int i=0, tmp=nums.size()-index-1; i<=index; i++, tmp++)
+//         new_nums[tmp] = nums[i];
 //
-//    int result = binarySearch(new_nums, target);
-//    if(result != -1)
-//        if(index)
-//            return result+index+1;
-//        else
-//            return result+index;
-//    else
-//        return result;
-//}
+//     int result = binarySearch(new_nums, target);
+//     if(result != -1)
+//         if(index)
+//             return result+index+1;
+//         else
+//             return result+index;
+//     else
+//         return result;
+// }
 /*
  * Optimize
  * 1. locate middle element in the right part array or left part array
  * 2. find the rule when the left and right pointer move
  */
-int search(vector<int>& nums, int target){
-    int left=0, right=nums.size()-1;
-    int result = -1;
-    while(left <= right){
-        int middle = (left + right) / 2;
-        // 1.
-        bool leftpart = nums[middle] >= nums[left];
-        if(nums[middle] == target){
-            result = middle;
-            break;
-        }
-        // 2.
-        if(leftpart) {
-            if(target > nums[middle] ||(target < nums[middle] && target < nums[left]))
-                left = middle + 1;
-            else if(target < nums[middle] && target >= nums[left])
-                right = middle - 1;
-        }else{
-            if(target > nums[middle] && target <= nums[right])
-                left = middle + 1;
-            else if(target < nums[middle] || (target > nums[middle] && target > nums[right]))
-                right = middle - 1;
-        }
-    }
-    return result;
-}
-
-int search(vector<int>& nums, int target) {
-    int left=0, right=nums.size()-1;
-    while(left <= right){
+int search(vector<int> &nums, int target)
+{
+    int left = 0, right = nums.size() - 1;
+    while (left <= right)
+    {
         int mid = (left + right) / 2;
-        if(nums[mid] == target)
+        if (nums[mid] == target)
             return mid;
-        if(nums[left] >  nums[right]){
-            bool leftpart = nums[mid] >= nums[left];
-            if(leftpart){
-                if(target >= nums[left] && target < nums[mid])
-                    right = mid - 1;
-                else
-                    left = mid + 1;
-            }else{
-                if(target > nums[mid] && target <= nums[right])
-                    left = mid + 1;
-                else
-                    right = mid - 1;
-            }
-        }else{
-            if(target > nums[mid])
+
+        // 1.
+        // rotated left portion
+        else if (nums[mid] >= nums[left])
+        {
+            // 2.
+            if (target > nums[mid] || target < nums[left])
                 left = mid + 1;
             else
                 right = mid - 1;
         }
+        // rotated right portion
+        else
+        {
+            // 2.
+            if (target < nums[mid] || target > nums[right])
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
     }
     return -1;
 }
-//int main(){
-//    vector<int> nums = {5,1,3};
-//    int ans = search(nums, 4);
-//    cout << "ans: " << ans << endl;
-//}
