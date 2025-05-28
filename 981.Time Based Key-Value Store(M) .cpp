0e3e6817413e45c80a57d03063929c1a668fdb4e
tammy228 <saintpaulstammy@gmail.com>
2025-05-29 07:50:32 +0800

    Update 981. to more intuitive method

--- 981.Time Based Key-Value Store(M) .cpp
+++ 981.Time Based Key-Value Store(M) .cpp
@@ -1,5 +1,5 @@
 #include <string>
-#include <map>
+#include <vector>
 #include <unordered_map>
 using namespace std;
 
@@ -7,30 +7,47 @@ using namespace std;
  * Optimize
  *
  * Use map of map to achieve multiple key single value pair
+ * Since the timestamp of set() will be strictly ascending, we can apply binary search for get()
+ * We know binary search method is to get closer and closer to the target num, since the get() require to return
+ * timestamp <= target_timestamp, we can keep assign the value if we found timestamp that is less than target_timestamp
  *
- * Time:
- *  set: std::map.insert() is O(logN), but if you have hint(in this case, if timestamp is in order), then O(1)
- *       since there are N elements. total : O(NlogN)
- *  get: O(logN) for single operation(binary search) , M times of get will be O(MlogN)
+ * Time
+ *  set: O(1)
+ *  get: O(logN)
  *
  * Space: O(N)
  */
-class TimeMap {
+class TimeMap
+{
 public:
-    unordered_map<string, map<int, string>> table;
-    TimeMap() {
-
+    unordered_map<string, vector<pair<string, int>>> table;
+    TimeMap()
+    {
     }
 
-    void set(string key, string value, int timestamp) {
-        table[key].insert({timestamp, value});
+    void set(string key, string value, int timestamp)
+    {
+        table[key].push_back({value, timestamp});
     }
 
-    string get(string key, int timestamp) {
-        auto iterator = table[key].upper_bound(timestamp);
-        if (iterator != table[key].begin())
-            return prev(iterator)->second;
-        else
-            return "";
+    string get(string key, int timestamp)
+    {
+        string ans = "";
+        if (!table.count(key))
+            return ans;
+
+        int left = 0, right = table[key].size() - 1;
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            if (table[key][mid].second <= timestamp)
+            {
+                ans = table[key][mid].first;
+                left = mid + 1;
+            }
+            else
+                right = mid - 1;
+        }
+        return ans;
     }
 };
